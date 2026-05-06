d7fe34bd311b0693ac198d64e3fbe0a3927ff60b
cfvm02 <cf.vargas@uniandes.edu.co>
2026-05-06 12:08:55 -0500

    Cambios Smart feature

--- lib/views/pages/user/tutor_catalog_page.dart
+++ lib/views/pages/user/tutor_catalog_page.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:g45_flutter/config/api_config.dart';
+import 'package:g45_flutter/models/tutor_summary.dart';
 import 'package:g45_flutter/services/analytics_service.dart';
 import 'package:g45_flutter/viewmodels/tutor_viewmodel.dart';
 import 'package:g45_flutter/widgets/tutor/tutor_card.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:g45_flutter/services/recent_viewed.dart';
 
 class CatalogPage extends StatefulWidget {
   const CatalogPage({super.key});
@@ -17,6 +23,8 @@ class _CatalogPageState extends State<CatalogPage> {
   String searchQuery = "";
   String? selectedFaculty;
   String selectedSort = "";
+  bool recommendationsEnabled = false;
+  List<TutorSummary> recommendedTutors = [];
   String normalize(String text) {
     return text
         .toLowerCase()
@@ -35,6 +43,47 @@ class _CatalogPageState extends State<CatalogPage> {
     "Ingeniería de Sistemas y Computación",
     "Matemáticas",
   ];
+  Future<void> loadRecommendations() async {
+    try {
+      await RecentViewedService().init();
+
+      final searchedIds = RecentViewedService().ids;
+
+      print("===== RECOMMENDATIONS DEBUG =====");
+      print("RECENT VIEWED IDS SENT: $searchedIds");
+      print("POST URL: ${ApiConfig.baseUrl}/recommendations?n_top=5");
+
+      if (searchedIds.isEmpty) {
+        print("No hay tutores vistos recientemente");
+        return;
+      }
+
+      final response = await http.post(
+        Uri.parse("${ApiConfig.baseUrl}/recommendations?n_top=5"),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(searchedIds),
+      );
+
+      print("RECOMMENDATION STATUS: ${response.statusCode}");
+      print("RECOMMENDATION BODY: ${response.body}");
+
+      if (response.statusCode == 200) {
+        final List data = jsonDecode(response.body);
+
+        setState(() {
+          recommendedTutors = data
+              .map((json) => TutorSummary.fromJson(json))
+              .toList();
+
+          recommendationsEnabled = true;
+        });
+
+        print("RECOMMENDED TUTORS COUNT: ${recommendedTutors.length}");
+      }
+    } catch (e) {
+      print("ERROR RECOMMENDATIONS: $e");
+    }
+  }
 
   //una vez abre el page corre todo esto
   @override
@@ -63,15 +112,15 @@ class _CatalogPageState extends State<CatalogPage> {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    final vm = Provider.of<TutorViewModel>(
-      context,
-    );
+    final vm = Provider.of<TutorViewModel>(context);
 
     if (vm.isLoading) {
       return Center(child: CircularProgressIndicator());
     }
 
-    final filteredTutors = vm.tutors.where((tutor) {
+    final sourceTutors = recommendationsEnabled ? recommendedTutors : vm.tutors;
+
+    final filteredTutors = sourceTutors.where((tutor) {
       final name = normalize(tutor.name ?? "");
       final major = normalize(tutor.major ?? "");
       final query = normalize(searchQuery);
@@ -93,6 +142,9 @@ class _CatalogPageState extends State<CatalogPage> {
         (a, b) => (a.sessionPrice ?? 0).compareTo(b.sessionPrice ?? 0),
       );
     }
+    //-------------------------------------------
+    // Recomendaciones (solo la primera vez que entra al page)
+    //--------------------------------------------
 
     return SingleChildScrollView(
       controller: _scrollController,
@@ -107,9 +159,10 @@ class _CatalogPageState extends State<CatalogPage> {
                   child: Text(
                     'Catálogo de Tutores',
                     style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: colorScheme.onSurface),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: colorScheme.onSurface,
+                    ),
                   ),
                 ),
               ],
@@ -157,52 +210,125 @@ class _CatalogPageState extends State<CatalogPage> {
                 Wrap(
                   spacing: 8,
                   runSpacing: 8,
+                  alignment: WrapAlignment.center,
                   children: [
                     ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          if (selectedSort == "rating") {
-                            selectedSort = "";
-                          } else {
-                            selectedSort = "rating";
-                          }
+                          recommendationsEnabled = false;
+                          selectedSort = selectedSort == "rating"
+                              ? ""
+                              : "rating";
                         });
+
                         AnalyticsService.instance.logEvent('filter_applied', {
                           'filter_name': 'mejor_rating',
                         });
                       },
                       style: ElevatedButton.styleFrom(
+                        elevation: 0,
                         backgroundColor: selectedSort == "rating"
                             ? colorScheme.primary
-                            : colorScheme.primary.withOpacity(0.2),
+                            : colorScheme.surfaceContainerHigh,
                         foregroundColor: selectedSort == "rating"
                             ? colorScheme.onPrimary
-                            : colorScheme.primary,
+                            : colorScheme.onSurface,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 18,
+                          vertical: 12,
+                        ),
                       ),
-                      child: Text("Mejor Ratings"),
+                      child: const Text(
+                        "Mejor Ratings",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
                     ),
+
                     ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          if (selectedSort == "price") {
-                            selectedSort = "";
-                          } else {
-                            selectedSort = "price";
-                          }
+                          recommendationsEnabled = false;
+                          selectedSort = selectedSort == "price" ? "" : "price";
                         });
+
                         AnalyticsService.instance.logEvent('filter_applied', {
                           'filter_name': 'precio',
                         });
                       },
                       style: ElevatedButton.styleFrom(
+                        elevation: 0,
                         backgroundColor: selectedSort == "price"
                             ? colorScheme.primary
-                            : colorScheme.primary.withOpacity(0.2),
+                            : colorScheme.surfaceContainerHigh,
                         foregroundColor: selectedSort == "price"
                             ? colorScheme.onPrimary
-                            : colorScheme.primary,
+                            : colorScheme.onSurface,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 18,
+                          vertical: 12,
+                        ),
                       ),
-                      child: Text("Precio"),
+                      child: const Text(
+                        "Precio",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () async {
+                        print("========== CLICK RECOMENDADOS ==========");
+
+                        setState(() {
+                          recommendationsEnabled = !recommendationsEnabled;
+
+                          // desactivar sorts
+                          selectedSort = "";
+
+                          // limpiar recomendaciones al apagar
+                          if (!recommendationsEnabled) {
+                            recommendedTutors = [];
+                          }
+                        });
+
+                        print(
+                          "recommendationsEnabled: $recommendationsEnabled",
+                        );
+
+                        // SOLO si quedó activo
+                        if (recommendationsEnabled) {
+                          await loadRecommendations();
+                        }
+
+                        AnalyticsService.instance.logEvent('filter_applied', {
+                          'filter_name': 'recomendados',
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: recommendationsEnabled
+                            ? colorScheme.primary
+                            : colorScheme.surfaceContainerHigh,
+                        foregroundColor: recommendationsEnabled
+                            ? colorScheme.onPrimary
+                            : colorScheme.onSurface,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 18,
+                          vertical: 12,
+                        ),
+                      ),
+                      child: const Text(
+                        "Recomendados",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
                     ),
                   ],
                 ),
@@ -221,7 +347,7 @@ class _CatalogPageState extends State<CatalogPage> {
                 ),
                 //Botones de filtro de Facultad [mapeados]
                 SizedBox(
-                  height: 45,
+                  height: 50,
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: facultadesMock.length,
@@ -241,24 +367,38 @@ class _CatalogPageState extends State<CatalogPage> {
                                 selectedFaculty = facultad;
                               }
                             });
+
                             AnalyticsService.instance.logEvent(
                               'filter_applied',
                               {'filter_name': facultad},
                             );
                           },
+
                           style: ElevatedButton.styleFrom(
+                            elevation: 0,
+
                             backgroundColor: isSelected
                                 ? colorScheme.primary
-                                : colorScheme.primary.withOpacity(0.2),
+                                : colorScheme.surfaceContainerHigh,
+
                             foregroundColor: isSelected
                                 ? colorScheme.onPrimary
-                                : colorScheme.primary,
+                                : colorScheme.onSurface,
+
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
+                              borderRadius: BorderRadius.circular(16),
                             ),
-                            padding: EdgeInsets.symmetric(horizontal: 16),
+
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 18,
+                              vertical: 12,
+                            ),
                           ),
-                          child: Text(facultad),
+
+                          child: Text(
+                            facultad,
+                            style: const TextStyle(fontWeight: FontWeight.w600),
+                          ),
                         ),
                       );
                     },
